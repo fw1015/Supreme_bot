255c39e3548a19e9a381f0edd46f9f8d2d055969
fw1015 <founder101598@gmail.com>
2021-07-03 11:42:08 +0100

    -Web scraping -Selenium

---
+++ main.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import timeit
+
+full_name = "Wan Fong Ching Founder"
+email = "founder101598@gmail.com"
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+chrome_driver_path = "/Users/Founder/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=options)
+driver.get("https://www.supremenewyork.com/shop/all")
+
+def bot():
+    element = driver.find_element_by_xpath("/html/body/div[2]/nav/ul/li[11]/a")
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Supreme®/Sea to Summit Self Inflating Sleeping Mat"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "commit"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "checkout now"))
+    )
+    element.click()
+
+    # name_field = driver.find_element_by_id("order_billing_name")
+    # name_field.send_keys(full_name)
+
+if __name__ == "__main__":
+    print(timeit.timeit(bot, number=1))
+
